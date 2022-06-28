a8d3d00cdaee7170a02910dbbd7059a84c489b7c
spellhaven <j_jisu@protonmail.com>
2022-06-28 16:03:23 +0900

    220628 1603 어. 커밋해.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const i = 1;
+    const ganzistorm = {
+      color: 'DarkBlue',
+      fontsize: '20px'
+    };
+      return ( 
+        <div>
+          {
+            // 10 + 20 == 30 && (<div>맞습니다</div>)
+            // 10 + 20 == 300 ? (<div>맞습니다</div>) : (<div>틀림 ㅋ</div>)
+
+            (() => { // function() 대신 () => 가능.
+              if (i==1) return (<div>i는 1입니다.</div>)
+              if (i==2) return (<div>i는 2입니다.</div>)
+              if (i==3) return (<div>i는 3입니다.</div>)
+
+            })() // React 쉬운 거 맞아? if문 쓰는데도 이렇게 괄호 덕지덕지인데
+
+          }
+
+          <div id = "hellotext">안녕하세요</div>
+          <div style = {ganzistorm}>반갑습니다</div>
+          <div className='classTest'>가입인사드립니다.</div>
+        </div>
+      )
+  }
 }
 
 export default App;
